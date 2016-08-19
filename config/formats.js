f2eeb3317ae658f54d2ae86eeba3495bb1d32fa1
azum4roll <azum4roll@users.noreply.github.com>
2016-08-20 03:20:53 +0800

    remove debug code
    rot in pieces, tiesoul

--- config/formats.js
+++ config/formats.js
@@ -2260,7 +2260,6 @@ exports.Formats = [
 			let restrictedCount = 0;
 			for (let i = 0; i < team.length; i++) {
 				let ability = team[i].ability;
-				return ["isTrademark = " + ability.isTrademark];
 				if (ability && ability.isTrademark) {
 					if (abilityTable[name]) {
 						return ["Your Pokémon must have different trademarked abilities.", "(You have more than one Pokémon with the " + abiility + " trademark)"];
