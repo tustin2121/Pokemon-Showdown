25cafa2dbe935a924d53c3d6c7a36761ed60cd19
azum4roll <rl8263@gmail.com>
2016-09-17 11:43:01 +0800

    make electric type not grounded in RTM instead of flying

---
+++ mods/reverse/scripts.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.BattleScripts = {
+	inherit: true,
+	pokemon: {
+		isGrounded: function (negateImmunity) {
+			if ('gravity' in this.battle.pseudoWeather) return true;
+			if ('ingrain' in this.volatiles) return true;
+			if ('smackdown' in this.volatiles) return true;
+			let item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball') return true;
+			if (!negateImmunity && this.hasType('Electric')) return false;
+			if (this.hasAbility('levitate') && !this.battle.suppressingAttackEvents()) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			return item !== 'airballoon';
+		},
+	},
+};
