d967e36218b3427f0f88c4c837ef81ed8b9385e1
azum4roll <azum4roll@users.noreply.github.com>
2016-12-24 13:39:35 +0800

    added a bunch of aliases

--- mods/tppextras/aliases.js
+++ mods/tppextras/aliases.js
@@ -3,6 +3,16 @@
 exports.BattleAliases = {
 	'helix': 'Omastar',
 	'dome': 'Kabutops',
+	'amber': 'Aerodactyl',
+	'root': 'Cradily',
+	'claw': 'Armaldo',
+	'armor': 'Bastiodon',
+	'skull': 'Rampardos',
+	'cover': 'Carracosta',
+	'plume': 'Archeops',
+	'sail': 'Aurorus',
+	'jaw': 'Tyrantrum',
 	'best': 'Typhlosion',
 	'kapow': 'Explosion',
+	'oldenbird': 'Phancero',
 };
