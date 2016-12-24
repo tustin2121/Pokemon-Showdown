288c5c6403e2b64f318a4a9dda4678d5f3c32373
azum4roll <azum4roll@users.noreply.github.com>
2016-12-24 14:13:10 +0800

    added phancero and RB:202

--- mods/tppextras/formats-data.js
+++ mods/tppextras/formats-data.js
@@ -1,4 +1,23 @@
 'use strict';
 
 exports.BattleFormatsData = {
+	////////////////////////////////////
+	// Prism Fakemon
+	////////////////////////////////////
+	phancero: {
+		randomBattleMoves: ["swordsdance", "bulkup", "earthquake", "shadowclaw", "rockslide", "fireblast"],
+		eventPokemon: [
+			{"generation": 3, "level": 5, "moves": ["watergun", "watergun", "skyattack"]},
+		],
+		isNonstandard: true,
+		tier: "Prism",
+	},
+	
+	////////////////////////////////////
+	// Gen 1 Glitchmon
+	////////////////////////////////////
+	rb202: {
+		isNonstandard: true,
+		tier: "Glitch",
+	}
 };
