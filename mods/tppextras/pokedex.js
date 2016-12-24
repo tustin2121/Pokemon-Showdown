cac1a81661ab0e463208e150e8dc3e7d3abaeee5
azum4roll <azum4roll@users.noreply.github.com>
2016-12-24 17:36:27 +0800

    changed PC 4S H to RB:202
    I guess species name has to match its ID BrokeBack

--- mods/tppextras/pokedex.js
+++ mods/tppextras/pokedex.js
@@ -21,7 +21,7 @@ exports.BattlePokedex = {
 	////////////////////////////////////
 	rb202: {
 		num: -202,
-		species: "PC 4S H",
+		species: "RB:202",
 		types: ["Nοrmal", "Grοund"],
 		baseStats: {hp: 232, atk: 147, def: 145, spa: 136, spd: 136, spe: 128},
 		abilities: {0: ""},
