81a7c856b0a6b1c8780cf178343981241847e244
azum4roll <azum4roll@users.noreply.github.com>
2016-12-24 13:37:07 +0800

    add phancero

--- mods/tppextras/pokedex.js
+++ mods/tppextras/pokedex.js
@@ -1,17 +1,33 @@
 'use strict';
 
 exports.BattlePokedex = {
-    /*
-	rb202: {
+	////////////////////////////////////
+	// Prism Fakemon
+	////////////////////////////////////
+	phancero: {
+		num: 9001,
+		species: "Phancero",
+		types: ["Ghost", "Flying"],
+		baseStats: {hp: 178, atk: 137, def: 57, spa: 85, spd: 65, spe: 128},
+		abilities: {0: "Download", 1: "Contrary"},
+		heightm: 1.5,
+		weightkg: 46.8,
+		color: "Black",
+		eggGroups: ["Undiscovered"],
+	},
+	
+	////////////////////////////////////
+	// Gen 1 Glitchmon
+	////////////////////////////////////
+	/* rb202: {
 		num: -202,
 		species: "PC 4S H",
 		types: ["Normal", "Ground"],
 		baseStats: {hp: 232, atk: 147, def: 145, spa: 136, spd: 136, spe: 128},
-		abilities: {0: "Fake Ground"},
+		abilities: {0: "Fake Types"},
 		heightm: 66.1,
 		weightkg: 975.3,
 		color: "Black",
 		eggGroups: ["Undiscovered"],
-    },
-    */
+	}, */
 };
